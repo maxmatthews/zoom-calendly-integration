bf9b0ab77d6635fe3d22ddd108c419e1a7e642a8
Max Matthews <max@mzmtech.com>
2023-01-16 18:03:56 -0500

    Add secrets example

---
+++ secretsExample.js
@@ -0,0 +1,8 @@
+export default {
+	zoomSecret: "J...(22 characters total)",
+	calendlyPAT: "longAssRandomPersonalAccessToken",
+	calendlyOrg: "https://api.calendly.com/organizations/A...",
+	sendgridAPIKey: "SG.o...",
+};
+
+//don't forget to remove "example" from the file name
